e4692fea730038fd11d4056d397dc48477b15347
Pawel Pikula <pawel.pikula@erlang-solutions.com>
2014-03-05 14:16:53 +0100

    new testcases for xmpp over websockets

--- tests/websockets_SUITE.erl
+++ tests/websockets_SUITE.erl
@@ -17,6 +17,7 @@
 -module(websockets_SUITE).
 -compile(export_all).
 
+-include_lib("exml/include/exml.hrl").
 -include_lib("escalus/include/escalus.hrl").
 -include_lib("common_test/include/ct.hrl").
 
@@ -25,12 +26,15 @@
 %%--------------------------------------------------------------------
 
 -define(REGISTRATION_TIMEOUT, 2).  %% seconds
+-define(NS_FRAMING, <<"urn:ietf:params:xml:ns:xmpp-framing">>).
+-define(NS_STREAM, <<"http://etherx.jabber.org/streams">>).
 
 all() ->
-    [{group, ws_chat}].
+    [{group, ws_protocol}].
 
 groups() ->
-    [{ws_chat, [sequence], [chat_msg]}].
+    [{ws_chat, [sequence], [chat_msg]},
+     {ws_protocol, [sequence], [stream_format, invalid_stream]}].
 
 suite() ->
     escalus:suite().
@@ -73,6 +77,52 @@ chat_msg(Config) ->
         end).
 
 %%--------------------------------------------------------------------
+%% New stream format
+%%--------------------------------------------------------------------
+
+stream_format(Config) ->
+    GeraltSpec = escalus_users:get_options(Config, geralt),
+    Host = proplists:get_value(host, GeraltSpec, <<"localhost">>),
+    {ok, Conn, _NewSpec} = escalus_connection:connect(GeraltSpec),
+    ok = escalus_connection:send(Conn, escalus_stanza:ws_stream_start(Host)),
+
+    StreamStart = escalus_connection:get_stanza(Conn, wait_for_open),
+    escalus_new_assert:assert(fun is_stream_start/1,StreamStart),
+    
+    Features = escalus_connection:get_stanza(Conn, wait_for_features),
+    escalus_new_assert:assert(fun is_stream_features_stanza/1,Features),
+
+    escalus_connection:stop(Conn),
+    ok.
+
+invalid_stream(Config) ->
+    GeraltSpec = escalus_users:get_options(Config, geralt),
+    {ok, Conn, _NewSpec} = escalus_connection:connect(GeraltSpec),
+
+    ok = escalus_connection:send(Conn, #xmlel{name="someinvalidtag"}),
+    Stanza = escalus_connection:get_stanza(Conn, wait_for_streamerror),
+    
+    % check error format  - should be <error> instad of <stream:error>
+    escalus_new_assert:assert(fun is_error/1, Stanza),
+    escalus_connection:stop(Conn),
+    ok.
+
+
+%%--------------------------------------------------------------------
 %% Helpers
 %%--------------------------------------------------------------------
+is_error(#xmlel{name = <<"error">>})->
+    true;
+is_error(_)->
+    false.
+
+is_stream_start(#xmlel{name = <<"open">>} = Stanza) ->
+    escalus_pred:has_ns(?NS_FRAMING, Stanza);
+is_stream_start(_) ->
+    false.
+
+is_stream_features_stanza(#xmlel{name = <<"features">>} = Stanza) -> 
+    escalus_pred:has_ns(?NS_STREAM,Stanza);
+is_stream_features_stanza(_) ->
+    false.
 
