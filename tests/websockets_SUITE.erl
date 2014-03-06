9353b6525fc21739658e56bd65fe9b6b79db2da7
Pawel Pikula <pawel.pikula@erlang-solutions.com>
2014-03-06 10:18:55 +0100

    check xmlns in messsage

--- tests/websockets_SUITE.erl
+++ tests/websockets_SUITE.erl
@@ -30,7 +30,8 @@
 -define(NS_STREAM, <<"http://etherx.jabber.org/streams">>).
 
 all() ->
-    [{group, ws_protocol}].
+    [{group, ws_chat},
+     {group, ws_protocol}].
 
 groups() ->
     [{ws_chat, [sequence], [chat_msg]},
@@ -69,7 +70,9 @@ chat_msg(Config) ->
     escalus:story(Config, [{alice, 1}, {geralt, 1}], fun(Alice, Geralt) ->
 
         escalus_client:send(Alice, escalus_stanza:chat_to(Geralt, <<"Hi!">>)),
-        escalus:assert(is_chat_message, [<<"Hi!">>], escalus_client:wait_for_stanza(Geralt)),
+        S = escalus_client:wait_for_stanza(Geralt),
+        escalus:assert(is_chat_message, [<<"Hi!">>], S),
+        escalus:assert(has_ns, [<<"jabber:client">>], S),
 
         escalus_client:send(Geralt, escalus_stanza:chat_to(Alice, <<"Hello!">>)),
         escalus:assert(is_chat_message, [<<"Hello!">>], escalus_client:wait_for_stanza(Alice))
@@ -92,6 +95,10 @@ stream_format(Config) ->
     Features = escalus_connection:get_stanza(Conn, wait_for_features),
     escalus_new_assert:assert(fun is_stream_features_stanza/1,Features),
 
+    ok = escalus_connection:send(Conn, escalus_stanza:ws_stream_end()),
+    Close = escalus_connection:get_stanza(Conn, wait_for_close),
+    escalus_new_assert:assert(fun is_stream_end/1, Close),
+
     escalus_connection:stop(Conn),
     ok.
 
@@ -101,9 +108,12 @@ invalid_stream(Config) ->
 
     ok = escalus_connection:send(Conn, #xmlel{name="someinvalidtag"}),
     Stanza = escalus_connection:get_stanza(Conn, wait_for_streamerror),
-    
     % check error format  - should be <error> instad of <stream:error>
     escalus_new_assert:assert(fun is_error/1, Stanza),
+
+    Close = escalus_connection:get_stanza(Conn, wait_for_close),
+    escalus_new_assert:assert(fun is_stream_end/1, Close),
+
     escalus_connection:stop(Conn),
     ok.
 
@@ -111,9 +121,14 @@ invalid_stream(Config) ->
 %%--------------------------------------------------------------------
 %% Helpers
 %%--------------------------------------------------------------------
-is_error(#xmlel{name = <<"error">>})->
-    true;
+is_error(#xmlel{name = <<"error">>} = Stanza)->
+    escalus_pred:has_ns(?NS_STREAM, Stanza);
 is_error(_)->
+    false.
+
+is_stream_end(#xmlel{name = <<"close">>} = Stanza) ->
+    escalus_pred:has_ns(?NS_FRAMING, Stanza);
+is_stream_end(_) ->
     false.
 
 is_stream_start(#xmlel{name = <<"open">>} = Stanza) ->
